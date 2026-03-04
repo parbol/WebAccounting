b4d63d790826f889af6b29b5c35bafe6245c2b0d
parbol <pablom@cern.ch>
2026-03-04 16:39:32 +0100

    almost there

--- IndicoAccounting.py
+++ IndicoAccounting.py
@@ -14,6 +14,7 @@ import getpass
 import datetime
 import dateutil
 from dateutil.relativedelta import relativedelta
+import schedule
 
 #Technical stuff
 NORMAL = '\033[95m'
@@ -21,6 +22,14 @@ OKBLUE = '\033[94m'
 GOOD = '\033[92m'
 ERROR = '\033[91m'
 ENDC = '\033[0m'
+
+#The list of rules
+rules = [['Sunday', '19:30', '20:30', 'IFCA/P0-017 - Sala Teresa Rodrigo Anoro (Sala de Juntas)', 'Instrumentation meeting']]
+
+#The global coordinates
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver.get("https://indico.ifca.es/rooms/book")
+wait = WebDriverWait(driver, 40)
 
 
 #############################################################
@@ -31,7 +40,7 @@ def printLog(color, message):
 
 
 #############################################################
-def setTheTime(wait, time1, time2):
+def setTheTime(time1, time2):
 
     times = [time1, time2]
     try:
@@ -49,7 +58,7 @@ def setTheTime(wait, time1, time2):
     return True
 
 #############################################################
-def setTheRoom(wait, room):
+def setTheRoom(room):
 
     try:
         buttomDate = wait.until(EC.element_to_be_clickable((By.XPATH, '//input[@placeholder="e.g. IT Amphitheatre"]')))
@@ -62,7 +71,7 @@ def setTheRoom(wait, room):
     return True
 
 #############################################################
-def setTheDate(wait, date):
+def setTheDate(date):
 
     try:
         buttomDate = wait.until(EC.element_to_be_clickable((By.XPATH, '//input[@placeholder="DD/MM/YYYY"]')))
@@ -75,7 +84,7 @@ def setTheDate(wait, date):
     return True
 
 #############################################################
-def sendButtom(wait):
+def sendButtom():
     
     try:
         buttom = wait.until(EC.element_to_be_clickable((By.XPATH, '//button[@class="ui primary button"]')))
@@ -87,7 +96,7 @@ def sendButtom(wait):
     return True
 
 #############################################################
-def sendConfirm(wait):
+def sendConfirm():
     
     try:
         buttom = wait.until(EC.presence_of_element_located((By.XPATH, '//button[@class="ui green circular icon button"]')))
@@ -99,7 +108,7 @@ def sendConfirm(wait):
     return True
 
 #############################################################
-def sendUser(wait):
+def sendUser():
     
     try:
         buttom = wait.until(EC.element_to_be_clickable((By.XPATH, '//div[@class="ui radio checkbox"]')))
@@ -111,7 +120,7 @@ def sendUser(wait):
     return True
 
 #############################################################
-def sendCreateBooking(wait, meetingName):
+def sendCreateBooking(meetingName):
     
     try:
         buttom2 = wait.until(EC.presence_of_element_located((By.XPATH, '//textarea[@id="finalfield-reason"]')))
@@ -125,7 +134,7 @@ def sendCreateBooking(wait, meetingName):
     return True
 
 #############################################################
-def availableRoom(driver, wait):
+def availableRoom():
     driver.implicitly_wait(3)
     try:
         driver.find_element(By.XPATH, '//i[@class="warning sign icon"]')
@@ -136,26 +145,26 @@ def availableRoom(driver, wait):
 
 
 #############################################################
-def makeBooking(driver, wait, date, time1, time2, room, meetingName):
+def makeBooking(date, time1, time2, room, meetingName):
     
     # 0 is unknown error
     # 1 is there are no matches for that time/date
     # 2 is good 
-    if not setTheDate(wait, date):
+    if not setTheDate(date):
         return 0
-    if not setTheTime(wait, time1, time2):
+    if not setTheTime(time1, time2):
         return 0
-    if not setTheRoom(wait, room):
+    if not setTheRoom(room):
         return 0
-    if not sendButtom(wait):
+    if not sendButtom():
         return 0
-    if not availableRoom(driver, wait):
+    if not availableRoom():
         return 1
-    if not sendConfirm(wait):
+    if not sendConfirm():
         return 0
-    if not sendUser(wait):
+    if not sendUser():
         return 0
-    if not sendCreateBooking(wait, meetingName):
+    if not sendCreateBooking(meetingName):
         return 0
     return 2
 
@@ -190,46 +199,75 @@ def processDate(day, N):
         dates.append(stringdate)
     return dates
 
+#############################################################
+def estimateDate(day):
+
+    theDay = 0
+    if day == 'Monday':
+        theDay = 0
+    elif day == 'Tuesday':
+        theDay = 1
+    elif day == 'Wednesday':
+        theDay = 2
+    elif day == 'Thursday':
+        theDay = 3
+    elif day == 'Friday':
+        theDay = 4
+    elif day == 'Saturday':
+        theDay = 5
+    else:
+        theDay = 6
+
+    today = datetime.date.today()
+    nextMonth = today + relativedelta(months=+2)
+    target = nextMonth + relativedelta(days=-1)
+    weekday = target.weekday()
+    if weekday != theDay:
+        printLog(GOOD, 'Nothing to do for this date and rule')
+        return []
+    stringdate = f'{target.day}/{target.month}/{target.year}'
+    return [stringdate]
+
+
+#############################################################
+def update_booking():
+ 
+    for rule in rules:    
+        
+        listOfDates = estimateDate(rule[0])
+        for date in listOfDates:          
+
+            printLog(GOOD, 'Booking: ' + rule[4] + ', in room: ' + rule[3] + ' on ' + date + ' from: ' + rule[1] + ' to: ' + rule[2])    
+            status = makeBooking(date, rule[1], rule[2], rule[3], rule[4])
+
+            if not status:
+                printLog(ERROR, 'Exiting...')
+                sys.exit()
+            if status == 1:
+                printLog(ERROR, 'Meeting already reserved')
+        
+            printLog(GOOD, 'Meeting was booked successfully')
+            driver.get("https://indico.ifca.es/rooms/book")
+
 
 #############################################################
 if __name__=='__main__':
 
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-    #driver.maximize_window()
-    driver.get("https://indico.ifca.es/rooms/book")
-    wait = WebDriverWait(driver, 40)
     printLog(GOOD, 'Please fill in your credentials')
     passa = getpass.getpass(GOOD + "Press enter when ready" + ENDC)
 
     #List of rules
-    rules = [['Tuesday', '19:30', '20:30', 'IFCA/P0-017 - Sala Teresa Rodrigo Anoro (Sala de Juntas)', 'Instrumentation meeting']]
     for i in rules:
         printLog(GOOD, 'Request to make a meeting with title: ' + i[4] + ', in room: ' + i[3] + ' from: ' + i[1] + ' to: ' + i[2])
 
+    schedule.every().day.at("16:35").do(update_booking)
+
     while True:
 
-        #Condition to trigger the booking (not implemented yet)
+        schedule.run_pending()
+        time.sleep(1)
 
-        for rule in rules:
-            
-            listOfDates = processDate(rule[0], 5)
-            
-            for date in listOfDates:
-            
-                status = makeBooking(driver, wait, date, rule[1], rule[2], rule[3], rule[4])
-    
-                if not status:
-                    printLog(ERROR, 'Exiting...')
-                    sys.exit()
-                if status == 1:
-                    printLog(ERROR, 'Meeting already reserved')
         
-                printLog(GOOD, 'Meeting was booked successfully')
-
-                driver.get("https://indico.ifca.es/rooms/book")
-                wait = WebDriverWait(driver, 40)
-
-
     # Close the browser
     driver.quit()
 
